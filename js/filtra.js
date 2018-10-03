355fee01ff8e475fb24d955ec4385381e35ad544
Ian-FR <iiaan.fr@gmail.com>
2018-10-02 23:25:19 -0300

    Implementado o algoritimo para filtrar clientes com expressões regulares

--- js/filtra.js
+++ js/filtra.js
@@ -3,8 +3,23 @@ var filterField = document.querySelector('#filtrar-tabela');
 filterField.addEventListener('input', function() {
   var pacientes = document.querySelectorAll('.paciente');
   
-  for (let i = 0; i < pacientes.length; i++) {
-    var nomePaciente = pacientes[i].querySelector('.info-nome').textContent;
+  if (this.value.length != 0) {
+    
+    for (let i = 0; i < pacientes.length; i++) {
+      var nomePaciente = pacientes[i].querySelector('.info-nome').textContent;
+      var expression = new RegExp(this.value.toLowerCase(), 'i');
+      
+      if (!expression.test(nomePaciente)) {
+        pacientes[i].classList.add('invisible');
+      } else {
+        pacientes[i].classList.remove('invisible');
+      }
+    }
+  } else {
+    for (let i = 0; i < pacientes.length; i++) {
+      pacientes[i].classList.remove('invisible');
+      
+    }
   }
 
 });
