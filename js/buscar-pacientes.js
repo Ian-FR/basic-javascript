52fd4398bf7424dfe643b60bb8044d3c6a875767
Ian-FR <iiaan.fr@gmail.com>
2018-10-03 20:31:56 -0300

    Implemented AJAX requests for new patients

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -2,12 +2,25 @@ var botaoAdd = document.querySelector('#buscar-pacientes');
 
 botaoAdd.addEventListener('click', function() {
   var xhr = new XMLHttpRequest();
+  
   xhr.open('get', 'https://api-pacientes.herokuapp.com/pacientes');
+  
   xhr.addEventListener('load', function () {
-    console.log(this.responseText);
+    var erroAjax = document.querySelector('#erro-ajax');
+    if (this.status == 200) {
+      erroAjax.classList.add('invisible');
+
+      var patients = JSON.parse(this.responseText);
+      
+      patients.forEach(patient => {
+        addPatient(patient);
+      });      
+    } else {
+      erroAjax.classList.remove('invisible');
+    }
+    
   });
+
   xhr.send();
 
-  // TODO: adicionar os pacientes buscados na lista
-  
 })
